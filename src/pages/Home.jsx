66c3a909c07905664f432e8cc3cb327160b0dd25
LouAntonio <lourencodala88@gmail.com>
2026-01-04 15:13:59 +0100

    featuredCars component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import useDocumentTitle from '../hooks/useDocumentTitle'
 import Hero from '../components/Hero'
 import TrustedVehicles from '../components/TrustedVehicles'
+import FeaturedCars from '../components/FeaturedCars'
 
 export default function Home() {
 	useDocumentTitle('Página Inicial - Caxiauto')
@@ -11,6 +12,7 @@ export default function Home() {
 			<Hero />
 			<main className="max-w-7xl mx-auto p-6">
 				<TrustedVehicles />
+				<FeaturedCars />
 			</main>
 		</>
 	)
